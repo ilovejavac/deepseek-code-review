094784fa07ce0d5987cc5827eda2b48f80e6d6e7
eicen <lc024437@gmail.com>
2025-04-20 22:32:56 +0800

    feat: 检出代码

--- deepseek-code-review-sdk/src/main/java/com/bugstack/middleware/sdk/DeepSeekCodeReview.java
+++ deepseek-code-review-sdk/src/main/java/com/bugstack/middleware/sdk/DeepSeekCodeReview.java
@@ -1,9 +1,29 @@
 package com.bugstack.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class DeepSeekCodeReview {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("run code review-test");
+
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD-1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuffer diffCode = new StringBuffer();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("exited with code:" + exitCode);
+
+        System.out.println("评审代码" + diffCode);
     }
 
 }
